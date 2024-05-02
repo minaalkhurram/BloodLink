b65ef3c3728f16318dd9ae7b38ecd4871cd8b760
minaalkhurram <minaalkhurram318@gmail.com>
2024-05-02 23:33:11 +0500

    login activity -font changed - small text font monserrat-light (download from attributes font family )

--- app/src/main/java/com/example/bloodlink/singupActivity.java
+++ app/src/main/java/com/example/bloodlink/singupActivity.java
@@ -1,8 +1,6 @@
 package com.example.bloodlink;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class loginActivity extends AppCompatActivity {
+public class singupActivity extends AppCompatActivity {
 
-
-    EditText userTxt,passTxt;
-    Button loginBtn,signupBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        init();
+        setContentView(R.layout.activity_singup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-        private void init()
-        {
-
-            userTxt=findViewById(R.id.userTxt);
-            passTxt=findViewById(R.id.passTxt);
-            loginBtn=findViewById(R.id.loginbtn);
-            signupBtn=findViewById(R.id.singUpbtn);
-        }
-
 }
