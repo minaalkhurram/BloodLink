0077082dfd2d17a1fb5cca45d93207cc47e66e30
minaalkhurram <minaalkhurram318@gmail.com>
2024-05-02 23:22:28 +0500

    login activity - front end complete

--- app/src/main/java/com/example/bloodlink/MainActivity.java
+++ app/src/main/java/com/example/bloodlink/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bloodlink;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
